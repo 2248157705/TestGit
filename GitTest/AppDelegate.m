f3b7c70a53a2c759b32a4d9c5e6b89e35f144680
YQF <yqf@yqfdemacbook-pro-2.local>
2016-04-18 16:28:08 +0800

    12

--- GitTest/AppDelegate.m
+++ GitTest/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"1223");
+    NSLog(@"456");
     return YES;
 }
 
