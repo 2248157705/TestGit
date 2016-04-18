9f4fe5fe799dde59ff0da93dd5d065b1be102e3e
YQF <yqf@YQFdeMacBook-Pro-2.local>
2016-04-18 10:09:33 +0800

    Initial commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by YQF on 16/4/18.
+//  Copyright © 2016年 YQF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by YQF on 16/4/18.
+//  Copyright © 2016年 YQF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
